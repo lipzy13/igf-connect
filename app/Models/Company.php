754a6e70e54e7d7d03527a93dcf6789b873e85db
lipzy13 <88047810+lipzy13@users.noreply.github.com>
2024-12-05 14:31:27 +0700

    Add companies and conferences table. Edit users table

---
+++ app/Models/Company.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'company_name',
+        'representative_name',
+        'address',
+        'company_logo',
+        'about_us',
+        'company_type',
+        'key_product_line',
+        'country',
+        'status',
+    ];
+}
---
+++ database/migrations/2024_12_05_043423_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('representative_name');
+            $table->string('address');
+            $table->string('company_logo')->nullable();
+            $table->string('about_us');
+            $table->integer('company_type');
+            $table->integer('key_product_line');
+            $table->string('country');
+            $table->string('status');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
